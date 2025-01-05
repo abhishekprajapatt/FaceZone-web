18d01c72d681793d9b557259524a1929b51f3d18
Abhishek Prajapatt <145788814+abhishekprajapati2006@users.noreply.github.com>
2025-01-05 19:40:53 +0530

    commit

--- frontend/src/components/CreatePost.jsx
+++ frontend/src/components/CreatePost.jsx
@@ -1,5 +1,4 @@
 import React, { useRef, useState } from 'react';
-import { Dialog, DialogContent, DialogHeader } from './ui/dialog';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { Textarea } from './ui/textarea';
 import { Button } from './ui/button';
@@ -9,6 +8,7 @@ import { toast } from 'sonner';
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import { setPosts } from '@/redux/postSlice';
+import { Dialog, DialogContent, DialogHeader } from './ui/dialog';
 
 const CreatePost = ({ open, setOpen }) => {
   const imageRef = useRef();
