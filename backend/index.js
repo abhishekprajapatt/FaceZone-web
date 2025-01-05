40da3a978628ce95c6db38a6620cb0299c770ab6
Abhishek Prajapatt <145788814+abhishekprajapati2006@users.noreply.github.com>
2025-01-05 18:28:14 +0530

    dist file

--- backend/index.js
+++ backend/index.js
@@ -7,13 +7,13 @@ import userRoute from './routes/user.route.js';
 import postRoute from './routes/post.route.js';
 import messageRoute from './routes/message.route.js';
 import { app, server } from './socket/socket.js';
-// import path from 'path';
+import path from 'path';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 //middlewares
 app.use(express.json());
@@ -30,10 +30,10 @@ app.use('/api/v1/user', userRoute);
 app.use('/api/v1/post', postRoute);
 app.use('/api/v1/message', messageRoute);
 
-// app.use(express.static(path.join(__dirname, '/frontend/dist')));
-// app.get('*', (_, res) => {
-//   res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
-// });
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+app.get('*', (_, res) => {
+  res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+});
 
 
 server.listen(PORT, () => {
